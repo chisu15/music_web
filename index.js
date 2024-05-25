64a931fbeff69c4a50bce418c163f1ae2c1ce7ea
chisu15 <trunghieudo1509@gmail.com>
2024-05-26 02:30:18 +0700

    config index & redirectUrl login

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.use(cors({
 // }));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();

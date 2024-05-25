ca6b7bb9771a1bb3cb4a110294ddb46c46f34532
chisu15 <trunghieudo1509@gmail.com>
2024-05-26 02:42:40 +0700

    config cors 2

--- index.js
+++ index.js
@@ -13,25 +13,20 @@ const port = process.env.PORT;
 const db = require('./config/db');
 
 db.connect();
-//  DEV
+
+// Cấu hình CORS cho phép các miền cần thiết
 app.use(cors({
   origin: ['http://localhost:3000', 'https://musicwebbyahm.vercel.app', 'https://music-web-orcin.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
-// DEPLOY
-// app.use(cors({
-//   origin: 'https://music-web-orcin.vercel.app/',
-//   credentials: true,
-// }));
+// Cookie và Body Parser
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
 
 // ROUTE
 const route = require('./routes/index.route');

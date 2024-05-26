96cb4811bc55b2905b19dafc6d1a58fb4b96de00
chisu15 <trunghieudo1509@gmail.com>
2024-05-26 12:29:07 +0700

    change index

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 require('dotenv').config();
 const app = express();
@@ -13,24 +12,36 @@ const port = process.env.PORT;
 const db = require('./config/db');
 
 db.connect();
-//  DEV
+
+// Middleware để thiết lập các tiêu đề CORS
+const allowedOrigins = ['http://localhost:3000', 'https://musicwebbyahm.vercel.app', 'https://music-web-orcin.vercel.app'];
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true
 }));
 
 // Cookie và Body Parser
 app.use(cookieParser());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+// Middleware log request và thiết lập các tiêu đề CORS
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Access-Control-Allow-Credentials', 'true');
-  // res.setHeader('Access-Control-Max-Age', '1800');
-  // res.setHeader('Access-Control-Allow-Headers', 'content-type');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   next();
 });
 
@@ -53,7 +64,6 @@ const swaggerJsDocOptions = {
     },
     servers: [
       {
-        // url: "http://localhost:8080/"
         url: "https://music-web-orcin.vercel.app/",
         description: "My API Documentation"
       }
